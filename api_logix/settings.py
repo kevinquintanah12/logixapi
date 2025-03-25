45715feb92d19db42c8fd572632f6eb236015b78
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-03-25 01:01:49 -0600

    url3

--- api_logix/settings.py
+++ api_logix/settings.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'api_logix.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/logix_db',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
