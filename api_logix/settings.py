ad2d6495ef91a7a72e81b11f9c7def908f130e41
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-03-25 00:25:21 -0600

    settings

--- api_logix/settings.py
+++ api_logix/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,15 +99,13 @@ WSGI_APPLICATION = 'api_logix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Para PostgreSQL
-        'NAME': 'logix_db',       # Nombre de tu base de datos
-        'USER': 'postgres',                       # Tu nombre de usuario de PostgreSQL
-        'PASSWORD': 'postgres',                # Tu contraseña de PostgreSQL
-        'HOST': 'localhost',                        # Si está en tu máquina local
-        'PORT': '5432',      
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/logix_db',
+        conn_max_age=600
+    )
 }
 
 
