fed6dffaa00796f082a45267334320f3ef1a6acf
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-04-18 03:36:16 -0600

    cambios

--- api_logix/routing.py
+++ api_logix/routing.py
@@ -1,6 +1,6 @@
 # api_logix/routing.py
 from django.urls import re_path
-from graphql_subscriptions import MyGraphqlWsConsumer
+from api_logix.consumer import MyGraphqlWsConsumer
 
 websocket_urlpatterns = [
     # monta el consumer en ws://<host>/graphql/
