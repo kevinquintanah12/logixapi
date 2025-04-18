608fbd87b354a205833d84dedaf8f1e78b0e9b20
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-04-18 03:59:00 -0600

    websockets

--- api_logix/consumer.py
+++ api_logix/consumer.py
@@ -1,11 +1,11 @@
 # api_logix/consumers.py
 import channels_graphql_ws
-from entrega.schema import schema   # tu schema de GraphQL
-from rutas.schema import schema   # tu schema de GraphQL
+from entrega.schema import schema as entrega_schema
+from rutas.schema import schema as rutas_schema
 
 class MyGraphqlWsConsumer(channels_graphql_ws.GraphqlWsConsumer):
-    schema = schema
+    schema = entrega_schema  # Puedes fusionar los dos si quieres más adelante
+    schema = rutas_schema # Puedes fusionar los dos si quieres más adelante
 
     async def on_connect(self, payload):
-        # aquí puedes validar el token JWT si quieres
         return
