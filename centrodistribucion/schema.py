6cba52a3e8b43c9d07d30e7ec457279cebbb25c1
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-05-02 20:13:17 -0600

    cambios

--- centrodistribucion/schema.py
+++ centrodistribucion/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from .models import CentroDistribucion
-from Ubicacion.models import Ubicacion  # Importa el modelo Ubicacion
+from Ubicacion.models import Ubicacion
 from Ubicacion.schema import UbicacionType
 
 class CentroDistribucionType(DjangoObjectType):
@@ -20,7 +20,7 @@ class Query(graphene.ObjectType):
         return CentroDistribucion.objects.get(id=id)
 
 # Mutaciones
-class DarAltaCentroDistribucion(graphene.Mutation):
+class CrearCentroDistribucion(graphene.Mutation):
     class Arguments:
         ubicacion_id = graphene.Int(required=True)
 
@@ -29,12 +29,46 @@ class DarAltaCentroDistribucion(graphene.Mutation):
     def mutate(self, info, ubicacion_id):
         try:
             ubicacion = Ubicacion.objects.get(id=ubicacion_id)
-            centro_distribucion = CentroDistribucion.objects.create(
-                ubicacion=ubicacion
-            )
-            return DarAltaCentroDistribucion(centro_distribucion=centro_distribucion)
+            centro = CentroDistribucion.objects.create(ubicacion=ubicacion)
+            return CrearCentroDistribucion(centro_distribucion=centro)
         except Ubicacion.DoesNotExist:
             raise Exception("Ubicación no encontrada")
 
+class ActualizarCentroDistribucion(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        ubicacion_id = graphene.Int(required=True)
+
+    centro_distribucion = graphene.Field(CentroDistribucionType)
+
+    def mutate(self, info, id, ubicacion_id):
+        try:
+            centro = CentroDistribucion.objects.get(id=id)
+            ubicacion = Ubicacion.objects.get(id=ubicacion_id)
+            centro.ubicacion = ubicacion
+            centro.save()
+            return ActualizarCentroDistribucion(centro_distribucion=centro)
+        except CentroDistribucion.DoesNotExist:
+            raise Exception("Centro de distribución no encontrado")
+        except Ubicacion.DoesNotExist:
+            raise Exception("Ubicación no encontrada")
+
+class EliminarCentroDistribucion(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    ok = graphene.Boolean()
+
+    def mutate(self, info, id):
+        try:
+            centro = CentroDistribucion.objects.get(id=id)
+            centro.delete()
+            return EliminarCentroDistribucion(ok=True)
+        except CentroDistribucion.DoesNotExist:
+            raise Exception("Centro de distribución no encontrado")
+
+# Mutaciones raíz
 class Mutation(graphene.ObjectType):
-    dar_alta_centro_distribucion = DarAltaCentroDistribucion.Field()
+    crear_centro_distribucion = CrearCentroDistribucion.Field()
+    actualizar_centro_distribucion = ActualizarCentroDistribucion.Field()
+    eliminar_centro_distribucion = EliminarCentroDistribucion.Field()
