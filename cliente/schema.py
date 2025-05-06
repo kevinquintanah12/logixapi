2b4f0d9832264753a4ca2eb432ef071d6dc01b2b
kevinquintanah12 <kevinherreraq12@gmail.com>
2025-05-06 09:01:42 -0600

    cambios

--- cliente/schema.py
+++ cliente/schema.py
@@ -1,36 +1,67 @@
+# schema.py
+
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Q
 from .models import Cliente
 
+# ---------------------------------------------------
+# 1) Tipo GraphQL para Cliente
+# ---------------------------------------------------
 class ClienteType(DjangoObjectType):
     class Meta:
         model = Cliente
         fields = "__all__"
 
+
+# ---------------------------------------------------
+# 2) Query: individual, listado, último y búsqueda parcial
+# ---------------------------------------------------
 class Query(graphene.ObjectType):
-    cliente = graphene.Field(ClienteType, id=graphene.Int(required=True))
-    ultimo_cliente = graphene.Field(ClienteType)
-    clientes = graphene.List(ClienteType)
+    cliente              = graphene.Field(ClienteType, id=graphene.Int(required=True))
+    ultimo_cliente       = graphene.Field(ClienteType)
+    clientes             = graphene.List(ClienteType)
+    buscar_clientes      = graphene.List(
+        ClienteType,
+        termino=graphene.String(required=True),
+    )
 
     def resolve_cliente(self, info, id):
         return Cliente.objects.get(id=id)
 
     def resolve_ultimo_cliente(self, info):
-        return Cliente.objects.order_by('-id').first()
+        return Cliente.objects.order_by("-id").first()
 
     def resolve_clientes(self, info):
         return Cliente.objects.all()
 
+    def resolve_buscar_clientes(self, info, termino):
+        # Busca en varios campos aunque sea fragmento
+        return Cliente.objects.filter(
+            Q(nombre__icontains=termino) |
+            Q(apellido__icontains=termino) |
+            Q(razon_social__icontains=termino) |
+            Q(rfc__icontains=termino) |
+            Q(direccion__icontains=termino) |
+            Q(codigo_postal__icontains=termino) |
+            Q(telefono__icontains=termino) |
+            Q(email__icontains=termino)
+        ).distinct()
+
+
+# ---------------------------------------------------
+# 3) Mutaciones para Cliente
+# ---------------------------------------------------
 class CrearCliente(graphene.Mutation):
     class Arguments:
-        nombre = graphene.String(required=True)
-        apellido = graphene.String(required=True)
-        razon_social = graphene.String()
-        rfc = graphene.String(required=True)
-        direccion = graphene.String(required=True)
+        nombre        = graphene.String(required=True)
+        apellido      = graphene.String(required=True)
+        razon_social  = graphene.String()
+        rfc           = graphene.String(required=True)
+        direccion     = graphene.String(required=True)
         codigo_postal = graphene.String(required=True)
-        telefono = graphene.String()
-        email = graphene.String()
+        telefono      = graphene.String()
+        email         = graphene.String()
 
     cliente = graphene.Field(ClienteType)
 
@@ -47,17 +78,18 @@ class CrearCliente(graphene.Mutation):
         )
         return CrearCliente(cliente=cliente)
 
+
 class ActualizarCliente(graphene.Mutation):
     class Arguments:
-        id = graphene.Int(required=True)
-        nombre = graphene.String()
-        apellido = graphene.String()
-        razon_social = graphene.String()
-        rfc = graphene.String()
-        direccion = graphene.String()
+        id            = graphene.Int(required=True)
+        nombre        = graphene.String()
+        apellido      = graphene.String()
+        razon_social  = graphene.String()
+        rfc           = graphene.String()
+        direccion     = graphene.String()
         codigo_postal = graphene.String()
-        telefono = graphene.String()
-        email = graphene.String()
+        telefono      = graphene.String()
+        email         = graphene.String()
 
     cliente = graphene.Field(ClienteType)
 
@@ -68,6 +100,7 @@ class ActualizarCliente(graphene.Mutation):
                 setattr(cliente, attr, value)
         cliente.save()
         return ActualizarCliente(cliente=cliente)
+
 
 class EliminarCliente(graphene.Mutation):
     class Arguments:
@@ -83,9 +116,17 @@ class EliminarCliente(graphene.Mutation):
         except Cliente.DoesNotExist:
             return EliminarCliente(ok=False)
 
-class Mutation(graphene.ObjectType):
-    crear_cliente = CrearCliente.Field()
-    actualizar_cliente = ActualizarCliente.Field()
-    eliminar_cliente = EliminarCliente.Field()
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
+class Mutation(graphene.ObjectType):
+    crear_cliente      = CrearCliente.Field()
+    actualizar_cliente = ActualizarCliente.Field()
+    eliminar_cliente   = EliminarCliente.Field()
+
+
+# ---------------------------------------------------
+# 4) Schema final
+# ---------------------------------------------------
+schema = graphene.Schema(
+    query=Query,
+    mutation=Mutation,
+)
